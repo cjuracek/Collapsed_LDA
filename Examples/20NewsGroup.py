eceb2941a96143a6c5ad50085490cc50cadf6f57
cjuracek <therealjuracek@gmail.com>
2021-03-13 10:26:31 -0500

    Renamed variable and added initial spaCy imports

--- Examples/20NewsGroup.py
+++ Examples/20NewsGroup.py
@@ -5,8 +5,13 @@ from src.inference import *
 from sklearn.datasets import fetch_20newsgroups
 from time import perf_counter
 
+import spacy
+import en_core_web_sm
+
 if __name__ == '__main__':
-    
+    nlp = en_core_web_sm.load(disable=['parser', 'textcat'])
+    #nlp = spacy.load('en_core_web_sm')
+
     # With version of sklearn below .22
     dataset = fetch_20newsgroups(shuffle=True, random_state=1, remove=('headers', 'footers', 'quotes'))
     id_to_tokens = {i: tokenize_doc(doc) for i, doc in enumerate(dataset['data'])}
@@ -18,11 +23,11 @@ if __name__ == '__main__':
     
     titles_to_tokens_stem = {title: stem_tokens(tokens) for title, tokens in id_to_tokens.items()}
 
-    unique_words = get_unique_words(titles_to_tokens_stem.values())
+    vocabulary = get_unique_words(titles_to_tokens_stem.values())
 
     # Run LDA
     start_time = perf_counter()
     topic, phi, theta = LatentDirichletAllocation(titles_to_tokens_stem, K=20, alpha=2/20, niter=10)
     end_time = perf_counter()
     print(f'Done in {(end_time - start_time):.2f}')
-    print(get_top_n_words(phi, 5, unique_words))
+    print(get_top_n_words(phi, 5, vocabulary))

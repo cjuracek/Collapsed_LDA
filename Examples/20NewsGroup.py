c5ff4202f3a5a683eee1b9a632565abccd8574dd
cjuracek <therealjuracek@gmail.com>
2021-03-13 14:22:01 -0500

    Removed sections on manually processing in favor of spaCy functionality

--- Examples/20NewsGroup.py
+++ Examples/20NewsGroup.py
@@ -12,25 +12,24 @@ if __name__ == '__main__':
 
     # With version of sklearn below .22
     dataset = fetch_20newsgroups(shuffle=True, random_state=1, remove=('headers', 'footers', 'quotes'))
-    id_to_tokens = {i: tokenize_doc(doc) for i, doc in enumerate(dataset['data'])}
 
-    nlp = spacy.load('en_core_web_sm', disable=['parser', 'textcat'])
-    doc_list = {i: nlp(article) for i, article in enumerate(dataset['data'][:5])}
-    doc_list = {i: preprocess_spacy_doc(article, STOP_WORDS) for i, article in doc_list.items()}
+    # Process the articles with spaCy
+    nlp = spacy.load('en_core_web_sm', disable=['parser', 'textcat', 'ner'])
+    id_to_tokens = {i: nlp(article) for i, article in enumerate(dataset['data'])}
+
+    # Remove the stop words and lemmatize
+    id_to_tokens = {i: preprocess_spacy_doc(article, STOP_WORDS) for i, article in id_to_tokens.items()}
 
     # Remove articles whose content is 'blah blah blah'
-    extra_words = ['maxaxaxaxaxaxaxaxaxaxaxaxaxaxax', 'said', 'also', 'would', 'get', 'say', 'go', 'do', 'one']
-    id_to_tokens = {title: remove_stop_words(tokens, extra_words=extra_words)
-                    for title, tokens in id_to_tokens.items() if 'blah' not in tokens}
+    # extra_words = ['maxaxaxaxaxaxaxaxaxaxaxaxaxaxax', 'said', 'also', 'would', 'get', 'say', 'go', 'do', 'one']
+    # id_to_tokens = {title: remove_stop_words(tokens, extra_words=extra_words)
+    #                 for title, tokens in id_to_tokens.items() if 'blah' not in tokens}
 
-    # Stem tokens
-    titles_to_tokens_stem = {title: stem_tokens(tokens) for title, tokens in id_to_tokens.items()}
-
-    vocabulary = get_unique_words(titles_to_tokens_stem.values())
+    vocabulary = get_unique_words(id_to_tokens.values())
 
     # Run LDA
     start_time = perf_counter()
-    topic, phi, theta = LatentDirichletAllocation(titles_to_tokens_stem, K=20, alpha=2/20, niter=10)
+    topic, phi, theta = LatentDirichletAllocation(id_to_tokens, K=20, alpha=2/20, niter=10)
     end_time = perf_counter()
     print(f'Done in {(end_time - start_time):.2f}')
     print(get_top_n_words(phi, 5, vocabulary))

64c05e7afe59820432928f5fcc4f4bf0340b783f
Cole Juracek <therealjuracek@gmail.com>
2024-02-11 16:39:43 -0500

    lda test fixture added

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+
+from collapsed_lda.lda import LatentDirichletAllocation
+
+
+@pytest.fixture()
+def lda():
+    """LDA test fixture with 2 docs and 2 topics"""
+    doc_to_tokens = {
+        "doc_1": ["alpha", "bravo", "charlie"],
+        "doc_2": ["alpha", "delta", "echo"],
+    }
+    lda = LatentDirichletAllocation(
+        doc_to_tokens=doc_to_tokens, K=2, alpha=1, beta=0.01
+    )
+    return lda

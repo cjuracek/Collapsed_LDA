a27bcf3a010a5fe854743d432a3540059db4ee74
Cole Juracek <therealjuracek@gmail.com>
2024-02-17 15:35:46 -0500

    additional tests added

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -1,6 +1,14 @@
 import string
 
-from collapsed_lda.utility.utility import filter_extremes, get_unique_words, tokenize_doc
+from nltk.corpus import stopwords
+
+from collapsed_lda.utility.utility import (
+    filter_extremes,
+    get_unique_words,
+    preprocess_spacy_doc,
+    remove_stop_words,
+    tokenize_doc,
+)
 
 # Test filter_extremes()
 
@@ -61,3 +69,41 @@ def test_tokenize_doc_irregular_whitespace_removed():
     test_doc = f"token1 {string.whitespace} \x03 token2"
     processed_doc = tokenize_doc(test_doc)
     assert not any([whitespace in processed_doc for whitespace in string.whitespace + "\x03"])
+
+
+# Test remove_stop_words()
+
+
+def test_remove_stop_words_removes_extra_words():
+    tokens = ["test", "extra", "word"]
+    extra_words = ["extra", "word"]
+    filtered_tokens = remove_stop_words(tokens=tokens, extra_words=extra_words)
+    assert not any([word in filtered_tokens for word in extra_words])
+
+
+def test_remove_stop_words_removes_numbers():
+    tokens = ["test", "100", "2014"]
+    filtered_tokens = remove_stop_words(tokens=tokens, remove_numbers=True)
+    assert filtered_tokens == ["test"]
+
+
+def test_stop_words_removes_top_fifty_words():
+    tokens = [token for token in stopwords.words("English")]
+    tokens.append("test")
+    filtered_tokens = remove_stop_words(tokens, tokens_have_quotes=True)
+    assert filtered_tokens == ["test"]
+
+
+# Test preprocess_spacy_doc()
+
+
+def test_preprocess_spacy_doc_removes_stop_words(nlp):
+    test_doc = nlp("Stop that!")
+    test_tokens = preprocess_spacy_doc(test_doc, stop_words=["stop"])
+    assert test_tokens == ["that"]
+
+
+def test_preprocess_spacy_doc_removes_short_lemmas(nlp):
+    test_doc = nlp("I was reading the paper.")
+    test_tokens = preprocess_spacy_doc(test_doc)
+    assert all([len(token) > 2 for token in test_tokens])
